7eeb3696079201447b383af29d80b52313daadc5
Daniel Muñoz Ruiz <Daniel@MacBook-Pro.local>
2017-07-07 21:13:18 +0200

    fix https

--- src/PokemonList/index.js
+++ src/PokemonList/index.js
@@ -9,7 +9,7 @@ class PokemonList extends Component {
   }
 
   componentWillMount(){
-    fetch('http://pokeapi.co/api/v2/pokemon?limit=151')
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
       .then(res => res.json())
       .then(res => { 
         this.setState({ 
